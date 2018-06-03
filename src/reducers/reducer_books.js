9ece0d5727b596d9257296540d02c95614a6c427
Blake Engquist <engquist_blake@yahoo.com>
2018-06-03 18:05:43 -0500

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Harry Potter", pages: 300 },
+    { title: "JavaScript: The Good Parts", pages: 250 },
+    { title: "Hello World", pages: 350 },
+    { title: "Lord of the Rings", pages: 543 }
+  ];
+}
